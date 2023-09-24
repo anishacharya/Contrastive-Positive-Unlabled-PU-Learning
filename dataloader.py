a6805fa36c17a9006bbb1626be65600f8542a1c1
Anish Acharya <anishacharya@utexas.edu>
2023-09-24 12:42:44 -0500

    +

--- dataloader.py
+++ dataloader.py
@@ -13,57 +13,11 @@ from lightly.transforms import SimCLRTransform  # we will stick with this for al
 root_dir = os.path.join(os.path.dirname(__file__), './data/')
 
 
-def get_transforms(dataset: str):
-	"""
-	:param dataset:
-	:return:
-	"""
-	if dataset == 'cifar10':
-		mean, std = [0.4914, 0.4822, 0.4465], [0.2470, 0.2435, 0.2616]
-		model_ip_shape = 32
-	else:
-		raise NotImplementedError
-
-	mv_transform = SimCLRTransform(
-		input_size=model_ip_shape,
-		cj_strength=0.5,
-		gaussian_blur=0.0
-	)
-	basic_transform = BasicTransform(
-		mean=mean,
-		std=std,
-		input_shape=model_ip_shape
-	)
-	return mv_transform, basic_transform
-
-
-class BasicTransform:
-	"""
-	Basic Transform
-	"""
-
-	def __init__(
-			self,
-			mean: List,
-			std: List,
-			input_shape: int
-	):
-		self.mean, self.std = mean, std
-		self.transform = transforms.Compose([
-			transforms.Resize(input_shape),
-			transforms.ToTensor(),
-			transforms.Normalize(mean=self.mean, std=self.std),
-		])
-
-	def __call__(self, x):
-		return self.transform(x)
-
-
 class DataManager:
 	"""
     @param data_config = config
     """
-
+	
 	def __init__(
 			self,
 			data_config: Dict,
@@ -75,7 +29,7 @@ class DataManager:
 		self.num_classes = self.data_config.get('num_classes')
 		self.train_batch_size = self.data_config.get('train_batch_size', 256)
 		self.test_batch_size = self.data_config.get('test_batch_size', 1000)
-
+		
 		# DDP -- divide batch into gpus
 		if gpu_strategy == "ddp":
 			print("DDP ~ so updating the batch sizes accordingly to split data into gpus")
@@ -83,12 +37,55 @@ class DataManager:
 			self.train_batch_size //= n_gpus
 			self.test_batch_size //= n_gpus
 		self.num_worker = self.data_config.get('num_worker', 1)
-
+		
 		# initialize attributes specific to dataset
 		self.num_channels, self.height, self.width = None, None, None
 		self.tr_dataset, self.te_dataset = None, None
 		self.sv_transform, self.mv_transform, self.basic_transform = None, None, None
-
+	
+	def get_transforms(self):
+		"""
+		:return:
+		"""
+		if self.data_set == 'cifar10':
+			mean, std = [0.4914, 0.4822, 0.4465], [0.2470, 0.2435, 0.2616]
+			model_ip_shape = 32
+			mv_transform = SimCLRTransform(
+				input_size=model_ip_shape,
+				cj_strength=0.5,
+				gaussian_blur=0.0
+			)
+		else:
+			raise NotImplementedError
+		
+		basic_transform = self.BasicTransform(
+			mean=mean,
+			std=std,
+			input_shape=model_ip_shape
+		)
+		return mv_transform, basic_transform
+	
+	class BasicTransform:
+		"""
+		Basic Transform
+		"""
+		
+		def __init__(
+				self,
+				mean: List,
+				std: List,
+				input_shape: int
+		):
+			self.mean, self.std = mean, std
+			self.transform = transforms.Compose([
+				transforms.Resize(input_shape),
+				transforms.ToTensor(),
+				transforms.Normalize(mean=self.mean, std=self.std),
+			])
+		
+		def __call__(self, x):
+			return self.transform(x)
+	
 	def get_contrastive_learning_data(self) -> [DataLoader, DataLoader, DataLoader]:
 		"""
         Returns:
@@ -110,21 +107,10 @@ class DataManager:
 				download=True,
 				train=False
 			)
-			# we will use the same transform across all methods for fair comparison of proposed losses
-			# augmentations proposed in SimCLR-v2 : https://github.com/google-research/simclr#pretraining
-			self.mv_transform = SimCLRTransform(
-				input_size=self.model_ip_shape,
-				cj_strength=0.5,
-				gaussian_blur=0.0
-			)
 		else:
 			raise NotImplementedError
 		# define validation and test transform
-		self.basic_transform = BasicTransform(
-			mean=self.mean,
-			std=self.std,
-			input_shape=self.model_ip_shape
-		)
+		self.mv_transform, self.basic_transform = self.get_transforms()
 		dataset_train_ssl.transform = self.mv_transform
 		dataset_train_val.transform = self.basic_transform
 		dataset_test.transform = self.basic_transform
