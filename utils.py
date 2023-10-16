6bb64287bd2d78bea3e46c183c72c16683bad71b
Anish Acharya <anishacharya@utexas.edu>
2023-10-15 21:24:35 -0500

    +

--- utils.py
+++ utils.py
@@ -5,8 +5,11 @@ from typing import Dict
 from torch import optim
 from lightly.utils.lars import LARS
 import transformers
+import torch.nn as nn
+import torch.nn.functional as F
 
 
+# ---- Optimizer ------
 def get_optimizer(
 		params,
 		optimizer_config: Dict = None
@@ -94,3 +97,156 @@ def get_scheduler(
 	else:
 		return None
 
+
+# ---- Models ------
+def conv3x3(in_planes, out_planes, stride=1):
+	"""
+
+	:param in_planes:
+	:param out_planes:
+	:param stride:
+	:return:
+	"""
+	return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+	expansion = 1
+	
+	def __init__(self, in_planes, planes, stride=1):
+		super(BasicBlock, self).__init__()
+		self.conv1 = conv3x3(in_planes, planes, stride)
+		self.bn1 = nn.BatchNorm2d(planes)
+		self.conv2 = conv3x3(planes, planes)
+		self.bn2 = nn.BatchNorm2d(planes)
+		
+		self.shortcut = nn.Sequential()
+		if stride != 1 or in_planes != self.expansion * planes:
+			self.shortcut = nn.Sequential(
+				nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+				nn.BatchNorm2d(self.expansion * planes)
+			)
+	
+	def forward(self, x):
+		"""
+
+		:param x:
+		:return:
+		"""
+		out = F.relu(self.bn1(self.conv1(x)))
+		out = self.bn2(self.conv2(out))
+		out += self.shortcut(x)
+		out = F.relu(out)
+		return out
+
+
+class Bottleneck(nn.Module):
+	expansion = 4
+	
+	def __init__(self, in_planes, planes, stride=1):
+		super(Bottleneck, self).__init__()
+		self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+		self.bn1 = nn.BatchNorm2d(planes)
+		self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+		self.bn2 = nn.BatchNorm2d(planes)
+		self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+		self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+		
+		self.shortcut = nn.Sequential()
+		if stride != 1 or in_planes != self.expansion * planes:
+			self.shortcut = nn.Sequential(
+				nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+				nn.BatchNorm2d(self.expansion * planes)
+			)
+	
+	def forward(self, x):
+		"""
+
+		:param x:
+		:return:
+		"""
+		out = F.relu(self.bn1(self.conv1(x)))
+		out = F.relu(self.bn2(self.conv2(out)))
+		out = self.bn3(self.conv3(out))
+		out += self.shortcut(x)
+		out = F.relu(out)
+		return out
+
+
+class CIFARResNet(nn.Module):
+	def __init__(self, block, num_blocks):
+		super(CIFARResNet, self).__init__()
+		self.in_planes = 64
+		
+		self.conv1 = conv3x3(3, 64)
+		self.bn1 = nn.BatchNorm2d(64)
+		self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+		self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+		self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+		self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+	
+	# self.linear = nn.Linear(512*block.expansion, num_classes)
+	
+	def _make_layer(self, block, planes, num_blocks, stride):
+		strides = [stride] + [1] * (num_blocks - 1)
+		layers = []
+		for stride in strides:
+			layers.append(block(self.in_planes, planes, stride))
+			self.in_planes = planes * block.expansion
+		return nn.Sequential(*layers)
+	
+	def forward(self, x):
+		"""
+
+		:param x:
+		:return:
+		"""
+		out = F.relu(self.bn1(self.conv1(x)))
+		out = self.layer1(out)
+		out = self.layer2(out)
+		out = self.layer3(out)
+		out = self.layer4(out)
+		out = F.avg_pool2d(out, 4)
+		# out = out.view(out.size(0), -1)
+		# out = self.linear(out)
+		return out
+
+
+def cifarresnet18():
+	"""
+
+	:return:
+	"""
+	return CIFARResNet(BasicBlock, [2, 2, 2, 2])
+
+
+def cifarresnet34():
+	"""
+
+	:return:
+	"""
+	return CIFARResNet(BasicBlock, [3, 4, 6, 3])
+
+
+def cifarresnet50():
+	"""
+
+	:return:
+	"""
+	return CIFARResNet(Bottleneck, [3, 4, 6, 3])
+
+
+def cifarresnet101():
+	"""
+
+	:return:
+	"""
+	return CIFARResNet(Bottleneck, [3, 4, 23, 3])
+
+
+def cifarresnet152():
+	"""
+
+	:return:
+	"""
+	return CIFARResNet(Bottleneck, [3, 8, 36, 3])
