8094d2a90d7048ce6709e9e88c7d91b3a63d2a66
Anish Acharya <anishacharya@utexas.edu>
2023-10-16 00:10:09 -0500

    +

--- losses.py
+++ losses.py
@@ -38,41 +38,17 @@ class SelfSupConLoss(nn.Module):
 		self.temperature = temperature
 		self.cross_entropy = nn.CrossEntropyLoss(reduction=reduction)
 	
-	def forward(self, z: torch.Tensor, z_aug: torch.Tensor, *kwargs) -> torch.Tensor:
+	def forward(self, z, z_aug, *kwargs):
 		"""
-		:param z: features
-		:param z_aug: augmentations
-		:return: loss value, scalar
+		:param z:
+		:param z_aug:
+		:param kwargs:
+		:return:
 		"""
-		# get inner product matrix - diag zero
-		batch_size, _ = z.shape
-		
-		# project onto hypersphere
-		z = nn.functional.normalize(z, dim=1)
-		z_aug = nn.functional.normalize(z_aug, dim=1)
-		
-		# calculate similarities block-wise - the resulting vectors have shape (batch_size, batch_size)
-		inner_pdt_00 = torch.einsum('nc,mc->nm', z, z) / self.temperature
-		inner_pdt_01 = torch.einsum('nc,mc->nm', z, z_aug) / self.temperature
-		inner_pdt_10 = torch.einsum("nc,mc->nm", z_aug, z) / self.temperature
-		inner_pdt_11 = torch.einsum('nc,mc->nm', z_aug, z_aug) / self.temperature
-		
-		# remove similarities between same views of the same image
-		diag_mask = torch.eye(batch_size, device=z.device, dtype=torch.bool)
-		inner_pdt_00 = inner_pdt_00[~diag_mask].view(batch_size, -1)
-		inner_pdt_11 = inner_pdt_11[~diag_mask].view(batch_size, -1)
-		
-		# concatenate blocks : o/p shape (2*batch_size, 2*batch_size) - diagonals (self sim) zero
-		# [ Block 01 ] | [ Block 00 ]
-		# [ Block 10 ] | [ Block 11 ]
-		inner_pdt_0100 = torch.cat([inner_pdt_01, inner_pdt_00], dim=1)
-		inner_pdt_1011 = torch.cat([inner_pdt_10, inner_pdt_11], dim=1)
-		logits = torch.cat([inner_pdt_0100, inner_pdt_1011], dim=0)
-		
-		labels = torch.arange(batch_size, device=z.device, dtype=torch.long)
-		labels = labels.repeat(2)
-		loss = self.cross_entropy(logits, labels)
-		
+		inner_pdt_mtx = compute_nll_mtx(z=z, z_aug=z_aug, temp=self.temp)
+		bs, _ = z.shape
+		labels = torch.arange(2 * bs, device=z.device, dtype=torch.long)
+		loss = self.cross_entropy(inner_pdt_mtx, labels)
 		return loss
 
 
@@ -96,18 +72,19 @@ class SupConLoss(nn.Module):
 		:return: loss value => scalar
 		"""
 		
-		nll_mtx = compute_nll_mtx(z=z, z_aug=z_aug, temp=self.temperature)
+		similarity_mtx = compute_nll_mtx(z=z, z_aug=z_aug, temp=self.temperature)
 		
 		# mask out contributions from samples not from same class as i
 		mask_label = torch.unsqueeze(labels, dim=-1)
 		eq_mask = torch.eq(mask_label, torch.t(mask_label))
 		eq_mask = torch.tile(eq_mask, (2, 2))
-		similarity_scores = nll_mtx * eq_mask
+		similarity_scores = similarity_mtx * eq_mask
 		
 		# compute the loss by averaging over multiple positives
 		loss = similarity_scores.sum(dim=1) / (eq_mask.sum(dim=1) - 1)
 		if self.reduction == 'mean':
 			loss = torch.mean(loss)
+		
 		return loss
 
 
