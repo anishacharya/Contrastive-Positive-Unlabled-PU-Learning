3491f296ccddc32355c29d32def8e5905f0f2ff6
Anish Acharya <anishacharya@utexas.edu>
2023-10-15 22:23:17 -0500

    +

--- dataloader.py
+++ dataloader.py
@@ -99,10 +99,8 @@ class DataManager:
         """
 		binary_class_mapping = {
 			# Cifar10 Class Mapping
-			# [
 			# 0:'airplane', 1:'automobile', 2:'bird', 3:'cat', 4:'deer',
 			# 5:'dog', 6:'frog', 7:'horse', 8:'ship', 9:'truck'
-			# ]
 			'cifar10.dog_cat': {
 				'pos_classes': [5],
 				'neg_classes': [3]
