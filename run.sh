a9b999423a9f4334ee97031ec8cc9cb752f39f6d
Anish Acharya <anishacharya@utexas.edu>
2023-10-20 19:09:17 -0500

    +

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-CKPT='/home/aa56927/CODE/ContrastivePULearning/logs/checkpoints/cifar10.dog_cat/pt-puCL-nP=2500/0/epoch=999.ckpt'
-
+#CKPT='/home/aa56927/CODE/ContrastivePULearning/logs/checkpoints/cifar10.dog_cat/pt-puCL-nP=2500/0/epoch=999.ckpt'
 # python3 run_linear_eval.py --mode lp --checkpoint $CKPT --exp_name lp-puCL-nnPU-puPL=5000
-python3 run_representation.py --exp_name case-control/nP=1k/bs=4096/lr=5/pt-puCL --dataset cifar10.1
+
+
+python3 run_representation.py --exp_name case-control/nP=1k/bs=4096/lr=5/pi=0.4/pt-ssCL --dataset cifar10.1
