d059f5118fad81f26fc88f113f75ecd2554a503a
Anish Acharya <anishacharya@utexas.edu>
2023-11-13 21:04:35 -0600

    +

--- dataloader.py
+++ dataloader.py
@@ -3,14 +3,16 @@ Data Loader Module
 """
 import os
 from typing import Dict, List
-from sklearn.utils import shuffle
+
+import lightly.data as data
+# from fastai.vision.all import *
 import numpy as np
 import torch
+from lightly.transforms import SimCLRTransform
+from skimage import color
+from sklearn.utils import shuffle
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-import lightly.data as data
-from lightly.transforms import SimCLRTransform
-# from fastai.vision.all import *
 
 root_dir = os.path.join(os.path.dirname(__file__), './data/')
 supported_binary_cifar_datasets = [
@@ -342,3 +344,131 @@ def binarize_dataset(
 	
 	features, targets = shuffle(features, targets, random_state=0)
 	return features, targets
+
+
+class ImageFolderInstance(datasets.ImageFolder):
+	"""Folder datasets which returns the index of the image as well
+	"""
+	
+	def __init__(self, root, transform=None, target_transform=None, two_crop=False):
+		super(ImageFolderInstance, self).__init__(root, transform, target_transform)
+		self.two_crop = two_crop
+	
+	def __getitem__(self, index):
+		"""
+		Args:
+			index (int): Index
+		Returns:
+			tuple: (image, target, index) where target is class_index of the target class.
+		"""
+		path, target = self.imgs[index]
+		image = self.loader(path)
+		if self.transform is not None:
+			img = self.transform(image)
+		if self.target_transform is not None:
+			target = self.target_transform(target)
+		
+		if self.two_crop:
+			img2 = self.transform(image)
+			img = torch.cat([img, img2], dim=0)
+		
+		return img, target, index
+
+
+class RGB2Lab(object):
+	"""Convert RGB PIL image to ndarray Lab."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2lab(img)
+		return img
+
+
+class RGB2HSV(object):
+	"""Convert RGB PIL image to ndarray HSV."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2hsv(img)
+		return img
+
+
+class RGB2HED(object):
+	"""Convert RGB PIL image to ndarray HED."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2hed(img)
+		return img
+
+
+class RGB2LUV(object):
+	"""Convert RGB PIL image to ndarray LUV."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2luv(img)
+		return img
+
+
+class RGB2YUV(object):
+	"""Convert RGB PIL image to ndarray YUV."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2yuv(img)
+		return img
+
+
+class RGB2XYZ(object):
+	"""Convert RGB PIL image to ndarray XYZ."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2xyz(img)
+		return img
+
+
+class RGB2YCbCr(object):
+	"""Convert RGB PIL image to ndarray YCbCr."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2ycbcr(img)
+		return img
+
+
+class RGB2YDbDr(object):
+	"""Convert RGB PIL image to ndarray YDbDr."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2ydbdr(img)
+		return img
+
+
+class RGB2YPbPr(object):
+	"""Convert RGB PIL image to ndarray YPbPr."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2ypbpr(img)
+		return img
+
+
+class RGB2YIQ(object):
+	"""Convert RGB PIL image to ndarray YIQ."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2yiq(img)
+		return img
+
+
+class RGB2CIERGB(object):
+	"""Convert RGB PIL image to ndarray RGBCIE."""
+	
+	def __call__(self, img):
+		img = np.asarray(img, np.uint8)
+		img = color.rgb2rgbcie(img)
+		return img
