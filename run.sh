a30efcb6d938836becef0e93d87ae8bd740ad40a
Anish Acharya <anishacharya@utexas.edu>
2023-11-17 02:11:15 -0600

    +

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # python3 run_linear_eval.py --mode lp --checkpoint $CKPT --exp_name lp-puCL-nnPU-puPL=5000
 
 
-python3 run_representation.py --exp_name nP=2.5k/mCL-pi=0.1 --dataset cifar10.dog_cat
+python3 run_representation.py --exp_name nP=2.5k/mCL-pi=0.3 --dataset cifar10.dog_cat
