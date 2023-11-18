3c8878bd7ad3fa4895f27fcc97eeb14c3113bf14
Anish Acharya <anishacharya@utexas.edu>
2023-11-18 16:27:22 -0600

    +

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # python3 run_linear_eval.py --mode lp --checkpoint $CKPT --exp_name lp-puCL-nnPU-puPL=5000
 
 
-python3 run_representation.py --exp_name gamma=0.5/ssCL --dataset imagenet
+python3 run_representation.py --exp_name gamma=0.2/ssCL --dataset imagenet
