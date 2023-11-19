6d57acd6cc781337d808fb2abc1422231e1b29d8
Anish Acharya <anishacharya@utexas.edu>
2023-11-19 08:14:05 -0600

    +

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 # LP / FT evaluate a pretrained model
-CKPT="/home/aa56927/CODE/ContrastivePULearning/logs/checkpoints/imagenet/gamma=0.5/puCL/0/epoch=99.ckpt"
-python3 run_linear_eval.py --mode lp --checkpoint $CKPT --exp_name gamma=0.5/nnPU --dataset imagenet
+#CKPT="/home/aa56927/CODE/ContrastivePULearning/logs/checkpoints/imagenet/gamma=0.5/puCL/0/epoch=99.ckpt"
+#python3 run_linear_eval.py --mode lp --checkpoint $CKPT --exp_name gamma=0.5/nnPU --dataset imagenet
+
+# train from scratch
+python3 run_linear_eval.py --mode ft --exp_name gamma=0.5/nnPU --dataset imagenet
 
 
 # Train contrastive encoder from scratch
