e502276dbe33ff5eee4da6114b128619863005e6
Anish Acharya <anishacharya@utexas.edu>
2023-11-21 05:31:42 -0600

    +

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 # LP / FT evaluate a pretrained model
-#CKPT="/home/aa56927/CODE/ContrastivePULearning/logs/checkpoints/cifar10.dog_cat/nP=5k/puCL/0/epoch=299.ckpt"
-#python3 run_linear_eval.py --mode lp --checkpoint $CKPT --exp_name nP=5k/nnPU --dataset cifar10.dog_cat
+CKPT="/home/aa56927/CODE/ContrastivePULearning/logs/checkpoints/cifar10.dog_cat/nP=5k/puCL/0/epoch=299.ckpt"
+python3 run_lp.py --mode lp --checkpoint $CKPT --exp_name nP=5k/puCL-nnPU --dataset cifar10.dog_cat
 
 #CKPT="/home/aa56927/CODE/ContrastivePULearning/logs/checkpoints/cifar10.dog_cat/nP=5k/puCL/0/epoch=299.ckpt"
 #python3 run_linear_eval.py --mode lp --checkpoint $CKPT --exp_name nP=5k/lp-puCL-pupL --dataset cifar10.dog_cat --puPL True
 
 # train from scratch
-python3 run_lp.py --mode ft --exp_name gamma=0.5/nnPU --dataset cifar10.dog_cat
+# python3 run_lp.py --mode ft --exp_name gamma=0.5/nnPU --dataset cifar10.dog_cat
 
 
 # Train contrastive encoder from scratch
