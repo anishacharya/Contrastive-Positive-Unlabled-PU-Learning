eddbd949fd0f0dfe5dd881ff163f2d39c1e0bf17
Anish Acharya <anishacharya@utexas.edu>
2023-11-21 20:32:02 -0600

    +

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ export CUDA_LAUNCH_BLOCKING=1
 #python3 run_linear_eval.py --mode lp --checkpoint $CKPT --exp_name nP=5k/lp-puCL-pupL --dataset cifar10.dog_cat --puPL True
 
 # ----- Embedding Visualization
-CKPT="logs/checkpoints/cifar10.dog_cat/nP=5k/puCL/0/epoch=299.ckpt"
-python3 run_tsne.py --checkpoint $CKPT --dataset cifar10.dog_cat --fig_name tsne_plots/nP=5k-puCL-ckpt=299
+#CKPT="logs/checkpoints/cifar10.dog_cat/nP=5k/puCL/0/epoch=299.ckpt"
+#python3 run_tsne.py --checkpoint $CKPT --dataset cifar10.dog_cat --fig_name tsne_plots/nP=5k-puCL-ckpt=299
 
 
 # train from scratch
@@ -17,5 +17,5 @@ python3 run_tsne.py --checkpoint $CKPT --dataset cifar10.dog_cat --fig_name tsne
 
 
 # Train contrastive encoder from scratch
-# python3 run_representation.py --exp_name gamma=0.05/sCL --dataset imagenet
+python3 run_representation.py --exp_name gamma=0.05/sCL --dataset imagenet
 #python3 run_representation.py --exp_name single_dataset/nP=500/sCL --dataset cifar10.dog_cat
