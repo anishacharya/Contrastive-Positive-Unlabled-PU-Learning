18bedaa78a40d4dfc193b510cf80f6b8b7ee4df4
Anish Acharya <anishacharya@utexas.edu>
2023-11-26 03:18:40 -0600

    +

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@
 #python3 run_linear_eval.py --mode ft --exp_name sup-ce  --dataset cifar10.hard
 
 # Train contrastive encoder from scratch
-python3 run_representation.py --exp_name single-dataset/gamma=0.4/pi=0.7-puNCE --dataset imagenet
+python3 run_representation.py --exp_name single-dataset/gamma=0.4/pi=0.9-puNCE --dataset imagenet
 #python3 run_representation.py --exp_name single_dataset/nP=500/sCL --dataset cifar10.dog_cat
 
 
