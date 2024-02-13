0689f056be50eddcaabbe652d1505bda8a978f4a
Anish Acharya <anishacharya@utexas.edu>
2024-02-13 01:01:51 -0600

    +

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@
 
 # Train contrastive encoder from scratch
 # python3 run_representation.py --exp_name case-control/nP=9k/puNCE-pi=0.9 --dataset imagenet
-python3 run_representation.py --exp_name case-control/nP=1k/puNCE-pi=0.7 --dataset cifar10.1
+python3 run_representation.py --exp_name case-control/nP=1k/puNCE-pi=0.9 --dataset cifar10.1
 
 
 
